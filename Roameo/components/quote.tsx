da3fe6adc58ec1ec66c7fcbc2c586f7a6ce6a16e
Mindula Perera <145087911+beast5292@users.noreply.github.com>
2024-12-17 01:41:30 +0530

    updated styles

--- Roameo/components/quote.tsx
+++ Roameo/components/quote.tsx
@@ -2,97 +2,98 @@ import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 const Quote = () => {
-
   return (
-    
-    <View style={styles.group68}>
-      <Text style={styles.hiSaman}>Hi Saman,</Text>
-      <Text style={styles.startHereText}>
-        <Text style={styles.span1}>Start</Text>
-        <Text style={styles.span2}> here.{"\n"}</Text>
-        <Text style={styles.span3}>Roam</Text>
-        <Text style={styles.span4}> everywhere.{"\n"}</Text>
-        <Text style={styles.span6}>Love</Text>
-        <Text style={styles.span8}> every mile.</Text>
-      </Text>
-      <Image
-        style={styles.profileImage}
+    <View style={styles.container}>
+      {/* Profile Section */}
+      <View style={styles.header}>
+        <Text style={styles.hiSaman}>Hi Saman,</Text>
+        <Image
+          style={styles.profileImage}
+          source={require("../assets/images/profile_pic.jpg")} 
+        />
+      </View>
 
-        source={require("../assets/images/profile_pic.jpg")}
+      <View style={styles.quoteContainer}>
+        <Text style={styles.quoteText}>
+          <Text style={styles.start}>Start</Text>
+          <Text style={styles.here}> here.{"\n"}</Text>
+          <Text style={styles.roam}>Roam</Text>
+          <Text style={styles.everywhere}> everywhere.{"\n"}</Text>
+          <Text style={styles.love}>Love</Text>
+          <Text style={styles.everyMile}> every mile.</Text>
+        </Text>
+      </View>
+
+      <Image
+        style={styles.earthImage}
+        source={require("../assets/images/earth_img.jpg")}
       />
     </View>
   );
-
 };
 
 const styles = StyleSheet.create({
-  group68: {
-    position: "relative",
-    padding: 16,
+  container: {
+    flex: 1,
+    backgroundColor: "#0A0A0A", 
+    paddingTop: 50,
   },
-  
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 20,
+  },
   hiSaman: {
     color: "#6b8292",
-    textAlign: "left",
-    fontFamily: "Inter-Bold",
     fontSize: 18,
     fontWeight: "700",
-    position: "absolute",
-    left: 2,
-    top: 14,
-  },
-  startHereText: {
-    textAlign: "left",
-    fontSize: 36,
-    fontWeight: "400",
-    position: "absolute",
-    left: 0,
-    top: 40,
-    width: 326,
-  },
-  span1: {
-    color: "#44cae9",
-    fontFamily: "Inter-Black",
-    fontSize: 36,
-    fontWeight: "900",
-  },
-  span2: {
-    color: "#ffffff",
-    fontFamily: "Inter-ExtraLight",
-    fontSize: 36,
-    fontWeight: "200",
-  },
-  span3: {
-    color: "#44cae9",
-    fontFamily: "Inter-Black",
-    fontSize: 36,
-    fontWeight: "900",
-  },
-  span4: {
-    color: "#ffffff",
-    fontFamily: "Inter-ExtraLight",
-    fontSize: 36,
-    fontWeight: "200",
-  },
-  span6: {
-    color: "#44cae9",
-    fontFamily: "Inter-Black",
-    fontSize: 36,
-    fontWeight: "900",
-  },
-  span8: {
-    color: "#ffffff",
-    fontFamily: "Inter-ExtraLight",
-    fontSize: 36,
-    fontWeight: "200",
+    fontFamily: "Inter-Bold",
   },
   profileImage: {
-    borderRadius: 50,
     width: 45,
     height: 45,
+    borderRadius: 25,
+    resizeMode: "cover",
+  },
+  quoteContainer: {
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  quoteText: {
+    fontSize: 36,
+    lineHeight: 42,
+    fontWeight: "400",
+  },
+  start: {
+    color: "#44cae9",
+    fontWeight: "900",
+  },
+  here: {
+    color: "#FFFFFF",
+    fontWeight: "200",
+  },
+  roam: {
+    color: "#44cae9",
+    fontWeight: "900",
+  },
+  everywhere: {
+    color: "#FFFFFF",
+    fontWeight: "200",
+  },
+  love: {
+    color: "#44cae9",
+    fontWeight: "900",
+  },
+  everyMile: {
+    color: "#FFFFFF",
+    fontWeight: "200",
+  },
+  earthImage: {
+    width: "100%",
+    height: 300,
     position: "absolute",
-    left: 361,
-    top: 0,
+    bottom: 0,
     resizeMode: "cover",
   },
 });

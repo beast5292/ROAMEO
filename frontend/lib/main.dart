a99859aa65da989143f276c3dcdd5fccd6cc9d4b
mahiirj <mahith.20210827@iit.ac.lk>
2024-12-28 15:53:04 +0530

    Firebase Setup Completed
    Flutter and Nest js apps are luanching
    Firebase setup completed
    Firestore database is connected to the app
    Three firebase applications created (android,ios and web)
    project email credentials in the text file

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:new_flu/firebase_options.dart';
 import 'package:new_flu/home_page.dart';
 import 'package:new_flu/profile_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
   runApp(const MyApp());
 }
 

1c40edd89335a30c1de634cb5da10483e72d6ebc
mahiirj <mahith.20210827@iit.ac.lk>
2025-01-25 00:42:50 +0530

    Compiling issues solved
    if it's not compiling run
    flutter clean
    flutter pub get
    in order to clean the dependencies and reload them to the application in a case of a compilation issue
    
    Google maps API is loading and working

--- frontend/android/app/src/main/kotlin/com/example/homepage/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/homepage/MainActivity.kt
@@ -1,6 +1,13 @@
-import io.flutter.plugins.googlemaps.GoogleMapsPlugin
+package com.example.homepage // Use your actual package name
 
-override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-    super.configureFlutterEngine(flutterEngine)
-    GoogleMapsPlugin.registerWith(flutterEngine) // Register the plugin
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // GoogleMapsPlugin is now automatically registered by Flutter's auto-registration mechanism.
+        // No need to manually call registerWith.
+    }
 }

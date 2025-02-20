deca1ad26621f48c374b8321649cc10bd029c81e
Sumana <fernandojoshua102@gmail.com>
2025-02-20 12:47:57 +0530

    Fixing stats comonent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StatsComponent } from "../stats/stats.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StatsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
---
+++ src/app/stats/stats.component.ts
@@ -0,0 +1,49 @@
+import { Component, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+@Component({
+  selector: 'app-stats',
+  templateUrl: './stats.component.html',
+  styleUrls: ['./stats.component.css']
+})
+export class StatsComponent implements AfterViewInit {
+  @ViewChildren('statBox') statBoxes!: QueryList<ElementRef>;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    // Register the ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Convert QueryList to an array
+    const columns = this.statBoxes.toArray().map(ref => ref.nativeElement);
+
+    // We'll create a timeline that controls all columns
+    // Each column will animate in from left to right as you scroll down,
+    // and reverse that animation as scroll up.
+    columns.forEach((col, index) => {
+      gsap.fromTo(
+        col,
+        {
+          x: -100,       // Start a bit left
+          opacity: 0
+        },
+        {
+          x: 0,
+          opacity: 1,
+          duration: 0.6,
+          scrollTrigger: {
+            trigger: col,
+            start: 'top 80%',   // Start anim when top of col hits 80% of viewport
+            end: 'top 30%',     // End anim when top of col hits 30% of viewport
+            scrub: true,        // Smoothly animate with scroll
+            // markers: true,   // Uncomment for debugging
+            onEnter: () => console.log(`Entering column ${index + 1}`),
+            onLeaveBack: () => console.log(`Leaving column ${index + 1}`)
+          }
+        }
+      );
+    });
+  }
+}

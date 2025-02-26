e2ae7eb46a69041efbb5921e0c9cb6c5814dbca8
Henuka De Silva <chanida.20220836@iit.ac.lk>
2025-02-26 23:31:22 +0530

    commit 13

---
+++ splashscreen/lib/screens/notifications_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class NotificationsPage extends StatelessWidget {
+  final List<Map<String, String>> notifications = [
+    {"type": "like", "user": "Alex", "message": "liked your post.", "image": "lib/assets/images/cars1.jpg"},
+    {"type": "comment", "user": "Sarah", "message": "commented: Nice post!", "image": "lib/assets/images/cars5.png"},
+    {"type": "follow", "user": "Michael", "message": "started following you.", "image": "lib/assets/images/Logo.png"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notifications"),
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+          final notification = notifications[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(notification["image"]!),
+            ),
+            title: RichText(
+              text: TextSpan(
+                style: TextStyle(color: Colors.white),
+                children: [
+                  TextSpan(text: notification["user"], style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: " ${notification["message"]}"),
+                ],
+              ),
+            ),
+            trailing: notification["type"] == "follow"
+                ? ElevatedButton(
+              onPressed: () {},
+              child: Text("Follow Back"),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            )
+                : null,
+          );
+        },
+      ),
+    );
+  }
+}

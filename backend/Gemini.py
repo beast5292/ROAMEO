f060b217e408f7a965b2069e4262b211ef833766
Sumana <fernandojoshua102@gmail.com>
2025-03-10 15:39:47 +0530

    Removed unnecessary folders

--- backend/Gemini.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCKmuBctSMoriGSx_-sIvl5UJibbkQnNCE")
-
-model = genai.GenerativeModel("gemini-1.5-pro")  # Use an available model
-response = model.generate_content("Explain how AI works")
-
-print(response.text)
-

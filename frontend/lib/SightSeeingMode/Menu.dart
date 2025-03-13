eb91fe076060fc9997fd1fc57d9f20c7b386d942
mahiirj <mahith.20210827@iit.ac.lk>
2025-03-13 21:19:16 +0530

    Revert changes for reordable list

--- frontend/lib/SightSeeingMode/Menu.dart
+++ frontend/lib/SightSeeingMode/Menu.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:practice/SightSeeingMode/CameraPage/pages/camera_page.dart';
 import 'package:practice/SightSeeingMode/Services/SightsSend.dart';
 import 'package:practice/SightSeeingMode/Sightseeing_mode_page.dart';
-import 'package:practice/SightSeeingMode/location_select/models/location_info.dart';
 import 'package:practice/SightSeeingMode/location_select/pages/autoCwidget.dart';
 import 'package:practice/SightSeeingMode/CameraPage/providers/Image_provider.dart';
 import 'package:practice/SightSeeingMode/location_select/providers/selected_place_provider.dart';
 import 'package:practice/SightSeeingMode/models/sight.dart';
-import 'package:practice/providers/combined_provider.dart';
-
 import 'package:provider/provider.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
@@ -41,62 +38,87 @@ class _SightMenuState extends State<SightMenu> {
   }
 
   onSightSave() async {
-    final combinedListProvider =
-        Provider.of<CombinedListProvider>(context, listen: false);
 
+    final selectedPlaceProvider =
+        Provider.of<SelectedPlaceProvider>(context, listen: false);
+
+    final selectedImageProvider =
+        Provider.of<SelectedImageProvider>(context, listen: false);
 
     //list to hold the Sightseeing mode object
     List<Sight> Sights = [];
 
+    print("Selected Locations: ${selectedPlaceProvider.selectedLocations}");
+    print("Selected Images: ${selectedImageProvider.selectedTrips}");
 
-    // Iterate through the combined list and create Sight objects
-    for (var item in combinedListProvider.combinedList) {
-      if (item is LocationInfo) {
-        Sight sight = Sight(
-          id: DateTime.now().millisecondsSinceEpoch.toString(),
-          name: item.prediction.mainText ?? "Unknown Place",
-          description: item.prediction.secondaryText ?? "No details available",
-          tags: ["dummyTag", "dummyTag2"],
-          lat: item.placeDetails.latitude,
-          long: item.placeDetails.longitude,
-          imageUrls: item.imageUrls.isNotEmpty ? item.imageUrls : [],
-        );
+    //iterate through selected locations and create Sight objects
+    for (var location in selectedPlaceProvider.selectedLocations) {
+      Sight sight = Sight(
+        id: DateTime.now()
+            .millisecondsSinceEpoch
+            .toString(), // Unique ID from the place API
+        name: location.prediction.mainText ?? "Unknown Place",
+        description:
+            location.prediction.secondaryText ?? "No details available",
+        tags: [
+          "dummyTag",
+          "dummyTag2"
+        ], // You can modify this to add tags if needed
+        lat: location.placeDetails.latitude,
+        long: location.placeDetails.longitude,
+        imageUrls: location.imageUrls.isNotEmpty ? location.imageUrls : [],
+      );
 
-        Sights.add(sight);
-      } else if (item is List<dynamic>) {
-        // Handle image trips
-        List<String> imagePaths = item
-            .where((imageData) =>
-                imageData.containsKey('photo') && imageData['photo'] != null)
-            .map<String>((imageData) => imageData['photo'] as String)
-            .toList();
+      print(sight.toString());
 
-        List<String> uploadedUrls = [];
-        for (String path in imagePaths) {
-          if (path.endsWith('.jpg')) {
-            String downloadUrl = await uploadImage(path);
-            uploadedUrls.add(downloadUrl);
-          }
+      Sights.add(sight);
+    }
+
+    for (var tripData in selectedImageProvider.selectedTrips) {
+      //Extracting image URLs
+      List<String> imagePaths = tripData
+          .where((imageData) =>
+              imageData.containsKey('photo') && imageData['photo'] != null)
+          .map<String>((imageData) => imageData['photo'] as String)
+          .toList();
+
+      //store the uploaded Urls (jpgs uploaded to the firestorage and google map links)
+      List<String> uploadedUrls = [];
+      for (String path in imagePaths) {
+        if (path.endsWith('.jpg')) {
+          //upload the image to firestorage
+          String downloadUrl = await uploadImage(path);
+
+          //add the firestorage url to the uploaded urls
+          uploadedUrls.add(downloadUrl);
         }
-
-        var firstImage = item.first;
-        double? lat = firstImage['latitude'] as double?;
-        double? long = firstImage['longitude'] as double?;
-
-        Sight sight = Sight(
-          id: DateTime.now().millisecondsSinceEpoch.toString(),
-          name: "Captured Image",
-          description: "Sightseeing image",
-          tags: ["dummy tag1", "dummy tag2"],
-          lat: lat,
-          long: long,
-          imageUrls: uploadedUrls,
-        );
-
-        Sights.add(sight);
       }
 
-    
+      //Extract lat and long from the first image in the trip
+      var firstImage = tripData.first;
+
+      double? lat;
+      double? long;
+
+      if (firstImage.containsKey('latitude') &&
+          firstImage.containsKey('longitude')) {
+        lat = firstImage['latitude'] as double?;
+        long = firstImage['longitude'] as double?;
+      }
+
+      Sight sight = Sight(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        name: "Captured Image",
+        description: "Sightseeing image",
+        tags: ["dummy tag1", "dummy tag2"],
+        lat: lat,
+        long: long,
+        imageUrls: uploadedUrls,
+      );
+
+      print(sight.toString());
+
+      Sights.add(sight);
     }
 
     //Print the entire Sights array after adding all objects
@@ -106,86 +128,108 @@ class _SightMenuState extends State<SightMenu> {
     sendSights(Sights);
   }
 
-   @override
+  
+  @override
   Widget build(BuildContext context) {
-    final combinedListProvider = Provider.of<CombinedListProvider>(context);
+    final selectedPlaceProvider = Provider.of<SelectedPlaceProvider>(context);
+    final selectedImageProvider = Provider.of<SelectedImageProvider>(context);
 
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Text(
-              "Create your own sightseeing mode",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
+            Text("Create your own sightseeing mode",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             SizedBox(height: 10),
 
-            // Reorderable list view
+            // Toggle switch
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Locations", style: TextStyle(fontSize: 16)),
+                Switch(
+                  value: showLocations,
+                  onChanged: (value) {
+                    setState(() {
+                      showLocations = value;
+                    });
+                  },
+                ),
+                Text("Images", style: TextStyle(fontSize: 16)),
+              ],
+            ),
+
             Expanded(
-              child: ReorderableListView(
-                onReorder: (oldIndex, newIndex) {
-                  // Handle reordering logic here
-                  combinedListProvider.reorderItems(oldIndex, newIndex);
-                },
-                children: combinedListProvider.combinedList.map<Widget>((item) {
-                  // Use a stable key based on the item's type and index
-                  final index = combinedListProvider.combinedList.indexOf(item);
-                  final key = ValueKey('${item.runtimeType}-$index');
+              child: showLocations
+                  ? ListView.builder(
+                      itemCount: selectedPlaceProvider.selectedLocations.length,
+                      itemBuilder: (context, index) {
+                        final location =
+                            selectedPlaceProvider.selectedLocations[index];
 
-                  if (item is LocationInfo) {
-                    // Display location item
-                    final firstImageUrl =
-                        item.imageUrls.isNotEmpty ? item.imageUrls[0] : null;
+                        // Get the first image URL from the Images list
+                        final firstImageUrl = location.imageUrls.isNotEmpty
+                            ? location.imageUrls[0]
+                            : null;
 
-                    return ListTile(
-                      key: key,
-                      leading: firstImageUrl != null
-                          ? Image.network(
-                              firstImageUrl,
-                              width: 40,
-                              height: 40,
-                              fit: BoxFit.cover,
-                            )
-                          : Icon(Icons.location_on, color: Colors.blue),
-                      title: Text(
-                        item.prediction.mainText ?? "Unknown Place",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                        return ListTile(
+                          leading: firstImageUrl != null
+                              ? Image.network(firstImageUrl,
+                                  width: 40,
+                                  height: 40,
+                                  fit: BoxFit.cover) // Smaller image size
+                              : Icon(Icons.location_on,
+                                  color: Colors
+                                      .blue), // Fallback icon if no image is available
+                          title: Text(
+                              location.prediction.mainText ?? "Unknown Place",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                          subtitle: Text(location.prediction.secondaryText ??
+                              "No details available"),
+                        );
+                      },
+                    )
+                  : selectedImageProvider.selectedTrips.isEmpty
+                      ? Center(
+                          child: Text("No images added yet",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey)))
+                      : ListView.builder(
+                          itemCount: selectedImageProvider.selectedTrips.length,
+                          itemBuilder: (context, index) {
+                            final tripData =
+                                selectedImageProvider.selectedTrips[index];
+
+                            if (tripData.isEmpty) {
+                              return SizedBox.shrink();
+                            }
+
+                            final imageDataList = tripData;
+
+                            return ListTile(
+                              title: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children:
+                                        imageDataList.map<Widget>((imageData) {
+                                      return Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Image.file(
+                                          File(imageData['photo']),
+                                          width: 150, // Smaller image size
+                                          height: 150,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
                         ),
-                      ),
-                      subtitle: Text(
-                        item.prediction.secondaryText ?? "No details available",
-                      ),
-                    );
-                  } else if (item is  List<Map<String, dynamic>>) {
-                    // Display image item
-                    return ListTile(
-                      key: key,
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: item.map<Widget>((imageData) {
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.file(
-                                  File(imageData['photo']),
-                                  width: 150,
-                                  height: 150,
-                                  fit: BoxFit.cover,
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        ],
-                      ),
-                    );
-                  } else {
-                    return SizedBox.shrink(); //Fallback for unknown types
-                  }
-                }).toList(),
-              ),
             ),
           ],
         ),
@@ -239,3 +283,6 @@ class _SightMenuState extends State<SightMenu> {
     );
   }
 }
+
+
+

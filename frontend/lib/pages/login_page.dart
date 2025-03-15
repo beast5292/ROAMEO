095b4761fbcb9904ce8cb295858ce6621462042f
Mindula Perera <145087911+beast5292@users.noreply.github.com>
2025-03-15 13:29:14 +0530

    Account setup page and Registration Complete page were added

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import '../Home/home_page.dart';
 import './sign_up_page.dart';
+import './setup_account_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -14,17 +14,12 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false;
-  // Storing the JWT token
   final storage = FlutterSecureStorage();
 
   Future<void> _login() async {
-    setState(() {
-      _isLoading = true;
-    });
-
+    setState(() => _isLoading = true);
     final url = Uri.parse('http://192.168.100.14:8000/login');
-
-    final Map<String, String> requestBody = {
+    final requestBody = {
       "email": _emailController.text,
       "password": _passwordController.text,
     };
@@ -36,105 +31,156 @@ class _LoginPageState extends State<LoginPage> {
         body: jsonEncode(requestBody),
       );
 
+      final responseBody = jsonDecode(response.body);
       if (response.statusCode == 200) {
-        // Parsing the response body to obtain the token
-        final responseBody = jsonDecode(response.body);
-        final String token = responseBody["token"];
-
-        // Saving the token
-        await storage.write(key: 'jwt_token', value: token);
-
+        await storage.write(key: 'jwt_token', value: responseBody["token"]);
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Login successful!')),
         );
-
-        // Navigating to the home page after successful login
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => HomePage()),
+          MaterialPageRoute(builder: (context) => SetupAccountPage()),
         );
       } else {
-        // Handling error messages from the backend
-        final responseBody = jsonDecode(response.body);
-        final errorMessage =
-            responseBody["detail"] ?? "Invalid credentials. Please try again.";
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(errorMessage)),
+          SnackBar(
+              content: Text(responseBody["detail"] ?? "Invalid credentials")),
         );
       }
-    } catch (error) {
+    } catch (_) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error: Unable to connect to the server')),
       );
     }
-
-    setState(() {
-      _isLoading = false;
-    });
+    setState(() => _isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-        backgroundColor: Colors.white,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Enter your email',
-                border: OutlineInputBorder(),
+      backgroundColor: Color.fromRGBO(3, 10, 14, 1),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40),
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: Colors.white.withOpacity(0.6),
+                  size: 35,
+                  shadows: [
+                    Shadow(color: Colors.white, blurRadius: 20),
+                    Shadow(
+                        color: Colors.white.withOpacity(0.5), blurRadius: 40),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Enter Your Password',
-                border: OutlineInputBorder(),
+              SizedBox(height: 90),
+              Text("Login",
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(68, 202, 233, 1))),
+              SizedBox(height: 40),
+              _buildTextField(
+                  Icons.email, "Enter your email / username", _emailController),
+              SizedBox(height: 30),
+              _buildTextField(
+                  Icons.lock, "Enter Your Password", _passwordController,
+                  obscureText: true),
+              SizedBox(height: 30),
+              Center(
+                child: ElevatedButton(
+                  onPressed: _isLoading ? null : _login,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(2, 32, 46, 1),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 150, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: _isLoading
+                      ? CircularProgressIndicator(color: Colors.white)
+                      : Text("Login",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                ),
               ),
-            ),
-            SizedBox(height: 24.0),
-            SizedBox(
-              width: double.infinity,
-              height: 50.0,
-              child: ElevatedButton(
-                onPressed: _isLoading ? null : _login,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+              SizedBox(height: 70),
+              Center(
+                  child: Text("or Login With",
+                      style: TextStyle(color: Colors.white70, fontSize: 16))),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildSocialButton(Icons.apple),
+                  SizedBox(width: 20),
+                  _buildSocialButton(Icons.facebook),
+                ],
+              ),
+              Spacer(),
+              Center(
+                child: Text.rich(
+                  TextSpan(
+                    text: "New user? ",
+                    style: TextStyle(color: Colors.white70),
+                    children: [
+                      WidgetSpan(
+                        child: GestureDetector(
+                          onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUpPage())),
+                          child: Text("Register Here",
+                              style: TextStyle(
+                                  color: Color.fromRGBO(68, 202, 233, 1),
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                child: _isLoading
-                    ? CircularProgressIndicator(color: Colors.white)
-                    : Text('Login'),
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignUpPage()),
-                );
-              },
-              child: Text(
-                'New user? Register Here',
-                style: TextStyle(color: Colors.blue),
-              ),
-            ),
-          ],
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField(
+      IconData icon, String hintText, TextEditingController controller,
+      {bool obscureText = false}) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.white70),
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.white70, fontSize: 14),
+        contentPadding: EdgeInsets.symmetric(vertical: 10),
+        filled: true,
+        fillColor: Color.fromARGB(166, 103, 102, 1118),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(22),
+            borderSide: BorderSide.none),
+      ),
+    );
+  }
+
+  Widget _buildSocialButton(IconData icon) {
+    return IconButton(
+      icon: Icon(icon, color: Color.fromRGBO(68, 202, 233, 1), size: 30),
+      onPressed: () {},
     );
   }
 }
---
+++ frontend/lib/pages/registration_complete_page.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import '../Home/home_page.dart';
+
+class RegistrationCompletePage extends StatelessWidget {
+  final String username; // Pass the username to this page
+
+  const RegistrationCompletePage({Key? key, required this.username})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 2, 12, 14),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment:
+                CrossAxisAlignment.start, // Left-align the content
+            children: [
+              const SizedBox(height: 130),
+              // Left-aligned title
+              const Text(
+                "Registration\nComplete",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w900,
+                  color: Color.fromRGBO(68, 202, 233, 1), // Title text color
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              // Left-aligned description
+              const Text(
+                "Account has been created successfully",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white70,
+                ),
+              ),
+              const Text(
+                "Go where your heart roams",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 80),
+
+              // Centered profile picture with glow effect
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      // Outershadow layer
+                      BoxShadow(
+                        color: Color.fromARGB(255, 20, 52, 66).withOpacity(0.6),
+                        blurRadius: 5,
+                        spreadRadius: 20,
+                      ),
+
+                      // Inner shadow layer
+                      BoxShadow(
+                        color:
+                            Color.fromARGB(255, 149, 200, 243).withOpacity(0.2),
+                        blurRadius: 4,
+                        spreadRadius: 8,
+                      ),
+                    ],
+                  ),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(
+                        'assets/profile_placeholder.png'), // Have to Replace with image logic
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Centered username
+              Center(
+                child: Text(
+                  username,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 140,
+              ),
+
+              // Centered Explore Home button
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 2, 32, 46),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 120, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    "Explore Home",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

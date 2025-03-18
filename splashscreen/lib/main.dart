b077129b2fb0e2db6313d3a83d3bb26cf1b30849
Henuka De Silva <chanida.20220836@iit.ac.lk>
2025-03-18 14:01:19 +0530

    removed the bloge page parts from this file and complete the splash screen

--- splashscreen/lib/main.dart
+++ splashscreen/lib/main.dart
@@ -1,37 +1,38 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/blogPage.dart';
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Blog App',
-      theme: ThemeData.dark(), // Use dark theme
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-      body: BlogPage(),
-    );
-  }
-}
+ import 'screens/splash_screen.dart';
+ 
+ void main() {
+   runApp(const MyApp());
+ }
+ 
+ class MyApp extends StatelessWidget {
+   const MyApp({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       title: 'Animated Splash Demo',
+       theme: ThemeData(
+         primarySwatch: Colors.blue,
+       ),
+       home: const SplashScreen(),
+       routes: {
+         '/home': (context) => const HomeScreen(),
+       },
+     );
+   }
+ }
+ 
+ class HomeScreen extends StatelessWidget {
+   const HomeScreen({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return const Scaffold(
+       body: Center(
+         child: Text('Main App Screen'),
+       ),
+     );
+   }
+ }

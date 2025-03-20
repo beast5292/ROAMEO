d0f5db464c7c8027447aa1e704307e8c33b1a164
Mindula Perera <145087911+beast5292@users.noreply.github.com>
2025-03-20 16:49:36 +0530

    new test added

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,15 +13,15 @@ client = TestClient(app)
 class TestFastAPI(unittest.TestCase):
     def test_add_sights(self):
         response = client.post("/sights/", json=[{
-            "name": "Eiffel Tower",
-            "description": "Famous landmark.",
-            "modeName": "Tourist Spot",
-            "modeDescription": "Iconic place to visit.",
+            "name": "Statue of Liberty",
+            "description": "Symbol of freedom and democracy.",
+            "modeName": "Historical Landmark",
+            "modeDescription": "A significant monument in the USA.",
             "username": "testuser",
-            "tags": ["historical", "popular"],
-            "lat": 48.8584,
-            "long": 2.2945,
-            "imageUrls": ["https://example.com/eiffel.jpg"]
+            "tags": ["monument", "USA", "history"],
+            "lat": 40.6892,
+            "long": -74.0445,
+            "imageUrls": ["https://example.com/statueofliberty.jpg"]
         }])
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()["message"], "Sightseeing mode added successfully")
@@ -33,7 +33,7 @@ class TestFastAPI(unittest.TestCase):
 
     def test_register_user(self):
         response = client.post("/signup", json={
-            "username": "beast5292",
+            "username": "beast1234",
             "email": "beast@example.com",
             "dob": "2004/01/04",
             "password": "beast123"

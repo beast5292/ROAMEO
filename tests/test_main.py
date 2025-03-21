f12c87bd4162262965357ec94e37f7624adb98bd
Mindula Perera <145087911+beast5292@users.noreply.github.com>
2025-03-21 08:57:24 +0530

    test_main.py file code changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import os
 import unittest
 from fastapi.testclient import TestClient
 
-# Ensure the backend folder is in the path
+# Confirming that the backend folder is in the path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from backend.main import app  
@@ -12,7 +12,7 @@ client = TestClient(app)
 
 class TestFastAPI(unittest.TestCase):
     def test_signup(self):
-        # Example data for testing signup
+        # Change this data when making push request, as it will be stored in firestore db
         user_data = {
             "username": "testuser",
             "email": "testuser@example.com",
@@ -22,10 +22,8 @@ class TestFastAPI(unittest.TestCase):
         
         response = client.post("/signup", json=user_data)
         
-        # Ensure the status code is 200
         self.assertEqual(response.status_code, 200)
         
-        # Ensure the response contains a message and a token
         self.assertIn("message", response.json())
         self.assertEqual(response.json()["message"], "User registered successfully")
         self.assertIn("token", response.json())
